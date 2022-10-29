6ea275937825c1d06bb9317d5e375515d3a90347
Daniel Hjartland <squashical@gmail.com>
2022-10-29 09:57:39 +0200

    Add function that loops over array and adds each element to the board

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gameBoard = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
-  const getBoard = () => board;
+  // const board = ["", "", "", "", "", "", "", "", ""];
+  const board = [["X", "O", "X"], ["O", "X", "O"], ["X", "O", "X"]];
+  const getBoard = (i,j) => board[i][j];
   const setBoard = (index, sign) => {
     board[index] = sign;
   }
+  return { getBoard, setBoard };
 })();
 
 const Player = (name, playerSign) => {
@@ -12,4 +14,20 @@ const Player = (name, playerSign) => {
   return { getName, getPlayerSign };
 };
 
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.setAttribute("square-index", `${i},${j}`);
+    square.textContent = gameBoard.getBoard(i,j);
+    
+    square.addEventListener("click", () => {
+      console.log(square.getAttribute("square-index"));
+    });
+    document.querySelector(".game-board").appendChild(square);
+  }
+}
 
+console.log(gameBoard.getBoard(0,0));
+gameBoard.setBoard((0,0), "O");
+console.log(gameBoard.getBoard(0,0));
